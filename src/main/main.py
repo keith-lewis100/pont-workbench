08e44a9efff82900ee3ab9ce106c7566b0333d46
Keith Lewis <klewis@cafex.com>
2017-08-23 11:18:42 +0100

    add grant form

--- src/main/main.py
+++ src/main/main.py
@@ -5,26 +5,37 @@ import wtforms
 
 app = Flask(__name__)
 
+grants = []
+
+class ProjectForm(wtforms.Form):
+    name = wtforms.StringField(validators=[wtforms.validators.InputRequired()])
+
 class MoneyForm(wtforms.Form):
-    currency = wtforms.RadioField(choices=[('sterling', u'£'), ('ugx', u'USh')])
+    currency = wtforms.RadioField(choices=[('sterling', u'£'), ('ugx', u'Ush')])
     value = wtforms.DecimalField()
+
+class GrantForm(wtforms.Form):
+    amount = wtforms.FormField(MoneyForm)
 
 @app.route('/')
 def home():
     project_url = '123456'
     return redirect(url_for('view_project', id=project_url))
     
-@app.route('/project/<id>', methods=['GET', 'POST'])
+@app.route('/project/<id>/', methods=['GET'])
 def view_project(id):
-    logging.info("view_project id=" + id)
+    app.logger.debug('view_project id=' + id)
     project = { 'name' : 'OVC-001', 'state': 'ACTIVE' }
-    amount = { 'currency': 'ugx' }
-    form = MoneyForm(request.form, obj=amount)
-    if request.method == 'POST' and form.validate():
-        logging.debug('amount=' + str(amount))
-        return redirect('/success')
-    return render_template('project.html', entity=project, form=form)
+    return render_template('project.html', entity=project)
 
-@app.route('/success')
-def new_grant():
-    return "Done"
+@app.route('/project/<id>/grants', methods=['GET', 'POST'])
+def view_grant_list(id):
+    app.logger.debug('view_project id=' + id)
+    grant = { 'amount': { 'currency': 'ugx' , 'value': 0 }}
+    add_form = GrantForm(request.form, obj=grant)
+    if request.method == 'POST' and add_form.validate():
+#        add_form.populate_obj(grant)
+        app.logger.debug('grant=' + str(grant))
+        grants.append(grant)
+        return redirect(url_for('view_grant_list', id=id))
+    return render_template('grants.html', entities=grants, add=add_form)
