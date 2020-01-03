e35cd52a0c682874bbd17050b5686a3a361888df
Keith Lewis <klewis@cafex.com>
2020-01-03 10:54:11 +0000

    fix funds bug

--- main/funds.py
+++ main/funds.py
@@ -30,7 +30,7 @@ def view_fund_list(db_id):
     form = FundForm(request.form, new_fund)
     model.add_form(ACTION_CREATE.name, form)   
     property_list = (name_field, code_field)
-    fund_list = db.Fund.query(db.Fund.committee == id).order(db.Fund.name).fetch()
+    fund_list = db.Fund.query(db.Fund.committee == db_id).order(db.Fund.name).fetch()
     return views.view_std_entity_list(model, 'Fund List', ACTION_CREATE,
                                       property_list, fund_list, committee)
 
