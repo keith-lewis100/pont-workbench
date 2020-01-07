dc887df974cc9a060b048543d6280c5492ef8ac8
Keith Lewis <klewis@cafex.com>
2020-01-07 13:22:31 +0000

    Add receent activity to dashboard

--- main/main.py
+++ main/main.py
@@ -3,8 +3,11 @@
 from flask import render_template
 
 from application import app
+import db
 import data_models
 import views
+import properties
+import renderers
 
 import funds
 import projects
@@ -20,6 +23,13 @@ import partners
 import foreign_transfer
 import committees
 
+audit_fields = [
+    properties.DateProperty('timestamp'),
+    properties.KeyProperty('entity', title_of=lambda e: e.key.kind()),
+    properties.StringProperty('message'),
+    properties.KeyProperty('user')
+]
+
 @app.route('/')
 def home():
     model = data_models.Model(None)
@@ -27,7 +37,12 @@ def home():
                 ('Committee', 'Show Committees'),
                 ('Supplier', 'Show Suppliers'),
                 ('User', 'Show Users'))
-    return render_template('layout.html', title='DashBoard', user=views.view_user_controls(model), links=links)
+    audit_list = db.AuditRecord.query().order(-db.AuditRecord.timestamp).iter(limit = 10)
+    sub_heading = renderers.sub_heading('Recent Activity')
+    table = views.view_entity_list(audit_list, audit_fields, selectable=False)
+    content = renderers.render_div(sub_heading, table)
+    return render_template('layout.html', title='DashBoard', user=views.view_user_controls(model), links=links,
+                           content=content)
 
 projects.add_rules(app)
 pledges.add_rules(app)
